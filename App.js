8d1e887f4832c86a35be9b67ed61d3e2087a4d08
theus-shit <dias.matheus16@escola.pr.gov.br>
2026-02-09 12:38:58 +0000

    ir

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import sobrenome from './src/telas/sobrenome';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>matheus</Text>
+      <sobrenome />
       <StatusBar style="auto" />
     </View>
   );
---
+++ sobrenome.js
@@ -0,0 +1,6 @@
+import react from "react";
+import {text} from "react-native"
+
+export default function sobrenome(){
+    return <text>Dias</text>
+}
