cede013d92c809bffa3c2527ac78bb32670c945d
theus-shit <dias.matheus16@escola.pr.gov.br>
2026-02-09 12:42:57 +0000

    rt

--- sobrenome.js
+++ sobrenome.js
@@ -1,6 +1,6 @@
-import react from "react";
-import {text} from "react-native";
+import React from "react";
+import {Text} from "react-native";
 
 export default function sobrenome(){
-    return <text>Dias</text>
+    return <Text>Dias</Text>
 }
