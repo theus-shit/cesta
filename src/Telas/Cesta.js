dfa218475ade4a4127ea1d76e9490b45ff6cae09
theus-shit <dias.matheus16@escola.pr.gov.br>
2026-02-23 13:43:27 +0000

    1

--- src/Telas/Cesta.js
+++ src/Telas/Cesta.js
@@ -1,17 +1,24 @@
 import React from "react";
-import { Text,Image, Dimensions, StyleSheet } from "react-native";
+import { Text,Image, Dimensions, StyleSheet, View } from "react-native";
 
 import topo from '../../assets/topo.png';
+import lopo from '../../assets/topo.png';
 
-const whidth= Dimensions.get('screen'). width;
+const width= Dimensions.get('screen').width;
 
 export default function Cesta(){
     return<>
-    <Image source={topo} />
-    <Text>Detalhes da cesta</Text>
-    <Text>Cesta de verduras</Text>
-    <Text>Jenny Jack Farm</Text>
-    <Text>Uma cesta comk produtos selecionados
+    <Image source={topo} style={estilos.topo}/>
+
+    <Text style={estilos.titulo}>Detalhes da cesta</Text>
+    <Text style={estilos.nome}>Cesta de verduras</Text>
+
+     <View style={estilos.Fazenda}>
+        <Image source={logo} style={estilos.imagemFazenda}/>
+         <Text style={estilos.nomeFazenda}>Jenny Jack Farm</Text>
+     </View>
+   
+    <Text style={estilos.descricao}>Uma cesta comk produtos selecionados
         cuidadosamente da fazenda direto pra sua cozinha.
     </Text>
       <Text>R$: 40,00</Text>
@@ -23,5 +30,50 @@ const estilos = StyleSheet.create({
 topo:{
     width:"100%",
     height: 578 / 768 * width
+}, 
+titulo:{
+    width:"100%",
+    position:"absolute",
+    textAlign:"center",
+    fontSize:16,
+    lineHeight:26,
+    color:"white",
+    fontWeight:"bold",
+    padding: 16,
+},
+cesta:{
+    paddingVertical:8,
+    paddingHorizontal:16,
+},
+nome:{
+    color:"#464646",
+    fontSize: 26,
+    lineHeight: 42,
+    fontWeight:"bold"
+},
+fazenda:{
+    flexDirection:"row",
+    paddingVertical: 12,
+},
+imagemFazenda:{
+    width:32,
+    height:32,
+},
+nomeFazenda:{
+    fontSize: 16,
+    lineHeight: 26,
+},
+descricao:{
+    color:"#A3A3A3",
+    fontSize: 16,
+    lineHeight: 26,
+},
+preco:{
+    color:"#2A9F85",
+    fontWeight:"bold",
+    fontSize: 26,
+    lineHeight: 46,
+    margim:8,
+
 }
 })
